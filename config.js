d478b4963ae7428693a18d92132f85feb32058c0
Peterelzevir <114345532+Peterelzevir@users.noreply.github.com>
2025-04-01 13:21:01 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  // Token bot Telegram dari BotFather
+  telegramToken: '7332933814:AAG9VvU6jri2PPMMrsyPwqi2L2Y670zruCg',
+  
+  // Token GitHub (dengan izin untuk membuat repo)
+  githubToken: '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',
+  
+  // Username GitHub
+  githubUsername: 'cokycokynanas',
+  
+  // ID Telegram admin yang diizinkan menggunakan bot
+  adminIds: [
+    5988451717, // Ganti dengan ID Telegram kamu
+    // Tambahkan ID admin lain jika diperlukan
+  ]
+};
